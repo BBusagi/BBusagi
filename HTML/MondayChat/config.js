42b2ad8693aaefdb0a77362a7f0c2e4df3d2ddd8
Riku <lz354000575@gmail.com>
2026-02-02 12:08:43 +0900

    Monday Chat - VRM import

---
+++ HTML/MondayChat/config.js
@@ -0,0 +1,16 @@
+window.MONDAY_CONFIG = {
+  vrm: {
+    modelUrl: "./models/monday.vrm",
+    pixelRatioMax: 1.5,
+    targetHeight: 1.9,
+    camera: {
+      fov: 28,
+      position: [0, 1.6, 2.0],
+      lookAtYRatio: 0.7,
+    },
+    model: {
+      yRotation: 0,
+      offset: [0, 0, 0],
+    },
+  },
+};
